e6cd01d096fa7f0039cf2d91ee4b54a2fe200b15
Marwa-Khan <marwakhan211@gmail.com>
2024-10-29 17:27:13 +0200

    US1-test2

--- spreadsheet.py
+++ spreadsheet.py
@@ -4,9 +4,12 @@ class SpreadSheet:
     def __init__(self):
         self._cells = {}
 
-    def set(self, cell: str, value: int) -> None:
+    def set(self, cell: str, value: str) -> None:
         self._cells[cell] = value
 
-    def evaluate(self, cell: str) -> int:
-        return int(self._cells[cell])
+    def evaluate(self, cell: str):
+        try:
+            return int(self._cells[cell])
+        except ValueError:
+            return "Error"
 
